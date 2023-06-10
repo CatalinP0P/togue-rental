2fca627ff783e8258c1da0dd739bc4f2053ba190
home <home@MacBook-Pro-home.local>
2023-06-10 17:25:44 +0300

    fix

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -8,7 +8,7 @@ export default function Testimonials() {
       </label>
       <div className="w-[50px] bg-orange-400 h-[4px] mx-auto"></div>
       <label className="max-w-[1000px] mx-auto text-xl text-gray-700 font-light text-center">
-        Our clients have experienced our service and results, and they're eager
+        Our clients have experienced our service and results, and they&apos;re eager
         to share their positive experiences with you.
       </label>
       <div className="max-w-[1360px] mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 gap-y-4"></div>
@@ -16,11 +16,11 @@ export default function Testimonials() {
       <div className="grid grid-cols-1 lg:grid-cols-2 max-w-[1360px] gap-8 mx-auto">
         <div className="bg-gray-100 shadow-2xl p-16 flex flex-col gap-8 h-full justify-between">
           <p className="text-xl font-semibold">
-            "Renting a fast JDM car was an unforgettable experience. The power
+            &quot;Renting a fast JDM car was an unforgettable experience. The power
             and performance were mind-blowing, and the staff made the process
             seamless. Driving the car was a thrilling adventure, with
             lightning-fast acceleration and precise handling. It was an
-            adrenaline rush that made me feel like a professional racer."
+            adrenaline rush that made me feel like a professional racer.&quot;
           </p>
           <div className="flex flex-row w-full justify-between items-end">
             <div className="flex flex-row gap-4">
@@ -39,11 +39,11 @@ export default function Testimonials() {
 
         <div className="bg-gray-100 shadow-2xl p-16 flex flex-col gap-8 h-full justify-between">
           <p className="text-xl font-semibold">
-            "I had the time of my life renting a fast JDM car. The sleek design
-            and raw power were absolutely incredible. The rental agency's
+            &quot;I had the time of my life renting a fast JDM car. The sleek design
+            and raw power were absolutely incredible. The rental agency&apos;s
             knowledgeable staff made everything easy, and the driving experience
-            was a thrilling rush. It's an adventure I'll never forget, and I
-            can't wait to do it again."
+            was a thrilling rush. It&apos;s an adventure I&apos;ll never forget, and I
+            can&apos;t wait to do it again.&quot;
           </p>
           <div className="flex flex-row w-full justify-between items-end">
             <div className="flex flex-row gap-4">
